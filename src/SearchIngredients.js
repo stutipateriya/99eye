5a615bfb0201e34aadb85a3156ffef0fe244a8e0
stutipateriya <75521370+stutipateriya@users.noreply.github.com>
2020-12-07 17:35:48 +0530

    update filter logic

--- src/SearchIngredients.js
+++ src/SearchIngredients.js
@@ -11,18 +11,25 @@ class SearchIngredients extends Component {
       param: "",
       filter: false,
       clickedCard: "",
+      allFilter: null,
+      fill: null,
+      result:false
     };
   }
 
   search = (params) => {
- 
     if (params.length >= 3) {
       this.handleFilter("clear");
       Axios.get(
         "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + params
       )
         .then((data) => {
-          this.setState({ allDrinks: data.data.drinks, filter: false });
+          console.log(data.data.drinks);
+          this.setState({
+            allDrinks: data.data.drinks,
+            filter: false,
+            param: params,
+          });
         })
 
         .catch((err) => console.log(err));
@@ -31,13 +38,25 @@ class SearchIngredients extends Component {
     }
   };
   handleFilter = (key) => {
+    const { allDrinks } = this.state;
     if (key === "clear") {
-      this.setState({ allDrinks: null, filter: false });
+      this.setState({ allDrinks: allDrinks, filter: false, fill: null });
     } else {
       this.setState({ filter: false });
       Axios.get("https://www.thecocktaildb.com/api/json/v1/1/filter.php?" + key)
         .then((data) => {
-          this.setState({ allDrinks: data.data.drinks }, () => {});
+          this.setState({ allFilter: data.data.drinks }, () => {
+            const { allFilter } = this.state;
+            let filteredData = [];
+            for (let i = 0; i < allFilter.length; i++) {
+              for (let j = 0; j < allDrinks.length; j++) {
+                if (allFilter[i].strDrink == allDrinks[j].strDrink) {
+                  filteredData.push(allFilter[i]);
+                }
+              }
+            }
+            this.setState({ fill: filteredData, filter: false });
+          });
         })
 
         .catch((err) => console.log(err));
@@ -48,13 +67,13 @@ class SearchIngredients extends Component {
       Axios.get(
         "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" +
           item.strDrink
-      )
-        .then((res) =>
-          this.setState({ allDrinks: res.data.drinks, filter: true })
-        );
+      ).then((res) =>
+        this.setState({ allDrinks: res.data.drinks, filter: true })
+      );
     });
   };
   render() {
+    const data = this.state.fill ? this.state.fill : this.state.allDrinks;
     return (
       <div>
         <Header
@@ -62,8 +81,8 @@ class SearchIngredients extends Component {
           handleFilter={(key) => this.handleFilter(key)}
         />
 
-        {this.state.allDrinks ? (
-          this.state.allDrinks.map((item, i) => {
+        {data ? (
+          data.map((item, i) => {
             return (
               <Card
                 key={i}
@@ -78,26 +97,38 @@ class SearchIngredients extends Component {
                 />
                 <Card.Body>
                   <Card.Text>
-                      <>
-                    <h6>Drink: {item.strDrink}</h6>
-                    {!this.state.filter && (
-                      <p style={{color:"blue"}}>Click for details</p>
-                    )}
+                    <>
+                      <h6>Drink: {item.strDrink}</h6>
+                      {!this.state.filter && (
+                        <p style={{ color: "blue" }}>Click for details</p>
+                      )}
 
-                    {this.state.filter && (
-                      <div>
-                        <p><b>Glass:</b> {item.strGlass}</p>
-                        <ul>
+                      {this.state.filter && (
+                        <div>
+                          <p>
+                            <b>Glass:</b> {item.strGlass}
+                          </p>
+                          <ul>
                             <h6>Ingredients</h6>
-                            
-                            {item.strIngredient1 && <li>{item.strIngredient1}</li>}
-                            {item.strIngredient2 && <li>{item.strIngredient2}</li>}
-                            {item.strIngredient3 && <li>{item.strIngredient3}</li>}
-                            {item.strIngredient4 && <li>{item.strIngredient4}</li>}
-                            {item.strIngredient5 && <li>{item.strIngredient5}</li>}
-                        </ul>
-                      </div>
-                    )}
+
+                            {item.strIngredient1 && (
+                              <li>{item.strIngredient1}</li>
+                            )}
+                            {item.strIngredient2 && (
+                              <li>{item.strIngredient2}</li>
+                            )}
+                            {item.strIngredient3 && (
+                              <li>{item.strIngredient3}</li>
+                            )}
+                            {item.strIngredient4 && (
+                              <li>{item.strIngredient4}</li>
+                            )}
+                            {item.strIngredient5 && (
+                              <li>{item.strIngredient5}</li>
+                            )}
+                          </ul>
+                        </div>
+                      )}
                     </>
                   </Card.Text>
                 </Card.Body>
@@ -107,6 +138,7 @@ class SearchIngredients extends Component {
         ) : (
           <h4 className="mt-4">Please enter valid keyword..!</h4>
         )}
+        {data && data.length == 0 && <h4>No result found</h4>}
       </div>
     );
   }
